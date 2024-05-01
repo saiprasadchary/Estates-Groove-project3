eb27a4a86e4022563913e56f0e7c61c14f47f16f
saiprasadchary <82382862+saiprasadchary@users.noreply.github.com>
2024-04-30 23:22:33 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,270 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <title>Estates Groove</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="keywords" content="">
+  <meta name="description" content="">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+
+  <link rel="stylesheet" href="css/magnific-popup.css">
+
+  <link rel="stylesheet" href="css/animate.min.css">
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+
+  <link rel="stylesheet" href="css/nivo-lightbox.css">
+  <link rel="stylesheet" href="css/nivo_themes/default/default.css">
+
+  <link rel="stylesheet" href="css/hover-min.css">
+  <link rel="stylesheet" href="css/flexslider.css">
+
+  <link rel="stylesheet" href="css/style.css">
+
+  <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,300,600' rel='stylesheet' type='text/css'>
+
+</head>
+
+<body id="top" data-spy="scroll" data-target=".navbar-collapse" data-offset="50">
+
+  <div class="preloader">
+    <div class="sk-spinner sk-spinner-pulse"></div>
+  </div>
+
+  <section id="home" class="parallax-section">
+  
+    <div class="gradient-overlay"></div>
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-offset-2 col-md-8 col-sm-12">
+          <a href="index_choose.php" class="wow fadeInUp btn btn-default hvr-bounce-to-top smoothScroll"
+            data-wow-delay="">REGISTER</a>
+          <h1 class="wow fadeInUp" data-wow-delay="">Estates Groove</h1>
+          <p class="wow fadeInUp" data-wow-delay="">Dreams Linked, Homes Created: Launch Your Buying or Selling Journey with Estates Groove.</p>
+          <a href="buyer_login.php" class="wow fadeInUp btn btn-default hvr-bounce-to-top smoothScroll"
+            data-wow-delay="">BUY A PROPERTY</a>
+          <a href="seller_login.php" class="wow fadeInUp btn btn-default hvr-bounce-to-top smoothScroll"
+            data-wow-delay="">LIST YOUR PROPERTY</a>
+        </div>
+
+      </div>
+    </div>
+  </section>
+  <!-- Feature section -->
+  <section id="feature" class="parallax-section">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-offset-2 col-md-8 col-sm-offset-1 col-sm-10">
+          <div class="wow fadeInUp section-title" data-wow-delay="">
+            <h2>Why Choose Us?</h2>
+          </div>
+        </div>
+
+        <div class="clearfix"></div>
+
+        <div class="col-md-4 col-sm-6 wow fadeInUp" data-wow-delay="">
+          <div class="feature-thumb">
+            <div class="feature-icon">
+            </div>
+            <h3>EASY TRANSACTIONS</h3>
+            <p>    
+              Join a broad community of buyers and sellers. Estates Groove links you to a diverse network, 
+              boosting your odds of discovering the perfect property match.
+            </p>
+          </div>
+        </div>
+
+        <div class="col-md-4 col-sm-6 wow fadeInUp" data-wow-delay="">
+          <div class="feature-thumb">
+            <div class="feature-icon">
+            </div>
+            <h3>EXPANSIVE NETWORK</h3>
+            <p>
+
+              Join a wide-ranging community of buyers and sellers. With Estates Groove, you'll tap into a diverse network, 
+              elevating your chances of discovering the ideal match for your property requirements.
+            </p>
+          </div>
+        </div>
+
+        <div class="col-md-4 col-sm-6 wow fadeInUp" data-wow-delay="">
+          <div class="feature-thumb">
+            <div class="feature-icon">
+            </div>
+            <h3>VERIFIED LISTINGS</h3>
+            <p>
+              Trust is essential. Estates Groove guarantees the authenticity of listings, fostering a secure environment 
+              for both buyers and sellers. Bid farewell to uncertainties and embrace verified property listings.
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+
+  <!-- About section -->
+  <section id="about" class="parallax-section">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-offset-2 col-md-8 col-sm-offset-1 col-sm-10">
+          <div class="wow fadeInUp section-title" data-wow-delay="">
+            <h2>Featured Properties</h2>
+            <h4>Reach out to Estates Groove to schedule a site visit to these luxurious mansions.</h4>
+          </div>
+        </div>
+
+        <div class="clearfix"></div>
+        <center>
+      <!--  <div class="wow fadeInUp col-md-3 col-sm-5" data-wow-delay="">
+          <img src="images/about-img.jpg" class="img-responsive" alt="About">
+        </div>-->
+
+        <div class="wow fadeInUp col-md-5 col-sm-7" data-wow-delay="">
+
+          <!-- flexslider -->
+          <div class="flexslider">
+            <ul class="slides">
+
+              <li>
+                <img src="images/slide-img1.jpg" alt="Flexslider">
+              </li>
+              <li>
+                <img src="images/slide-img2.jpg" alt="Flexslider">
+              </li>
+              <li>
+                <img src="images/slide-img3.jpg" alt="Flexslider">
+              </li>
+
+            </ul>
+          </div>
+        </div>
+
+        <div class="wow fadeInUp col-md-4 col-sm-12" data-wow-delay="">
+
+          <ul>
+            <li>One Million+ users</li>
+            <li>Most trusted Sellers</li>
+            <li>Verfied Property Images</li>
+          </ul>
+        </div>
+        </center>
+      </div>
+    </div>
+  </section>
+
+  <!-- Footer section -->
+  <footer>
+    <div class="container">
+      <div class="row">
+
+        <div class="wow fadeInUp col-md-4 col-sm-4" data-wow-delay="">
+          <h3>Office Address</h3>
+          <p>2450 CamelliaLane Lindbergh NE, 30324</p>
+        </div>
+
+        <div class="wow fadeInUp col-md-4 col-sm-4" data-wow-delay="">
+          <h3>Contact Detail</h3>
+          <p>123-456-7890</p>
+          <p>estatesgroove@company.com</p>
+        </div>
+
+        <div class="wow fadeInUp col-md-4 col-sm-4" data-wow-delay="">
+          <h3>Opening Hours</h3>
+          <strong>Monday - Firday</strong>
+          <p>11:00 AM - 10:00 PM</p>
+          <strong>Saturday - Sunday</strong>
+          <p>10:00 AM - 09:00 PM</p>
+        </div>
+
+      </div>
+    </div>
+  </footer>
+
+  <!-- Copyright section -->
+  <section id="copyright">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-8 col-sm-8 col-xs-8">
+          <p>Copyright © Prasad and Giridhar
+        </div>
+
+        <div class="col-md-4 col-sm-4 text-right">
+          <a href="#top" class="fa fa-angle-up smoothScroll gototop"></a>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  <!-- javscript js -->
+  <script src="js/jquery.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+
+  <script src="js/jquery.magnific-popup.min.js"></script>
+
+  <script src="js/jquery.sticky.js"></script>
+  <script src="js/jquery.backstretch.min.js"></script>
+
+  <script src="js/isotope.js"></script>
+  <script src="js/imagesloaded.min.js"></script>
+  <script src="js/nivo-lightbox.min.js"></script>
+
+  <script src="js/jquery.flexslider-min.js"></script>
+
+  <script src="js/jquery.parallax.js"></script>
+  <script src="js/smoothscroll.js"></script>
+  <script src="js/wow.min.js"></script>
+
+  <script src="js/custom.js"></script>
+
+<!-- Code injected by live-server -->
+<script>
+	// <![CDATA[  <-- For SVG support
+	if ('WebSocket' in window) {
+		(function () {
+			function refreshCSS() {
+				var sheets = [].slice.call(document.getElementsByTagName("link"));
+				var head = document.getElementsByTagName("head")[0];
+				for (var i = 0; i < sheets.length; ++i) {
+					var elem = sheets[i];
+					var parent = elem.parentElement || head;
+					parent.removeChild(elem);
+					var rel = elem.rel;
+					if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
+						var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
+						elem.href = url + (url.indexOf('?') >= 0 ? '&' : '?') + '_cacheOverride=' + (new Date().valueOf());
+					}
+					parent.appendChild(elem);
+				}
+			}
+			var protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
+			var address = protocol + window.location.host + window.location.pathname + '/ws';
+			var socket = new WebSocket(address);
+			socket.onmessage = function (msg) {
+				if (msg.data == 'reload') window.location.reload();
+				else if (msg.data == 'refreshcss') refreshCSS();
+			};
+			if (sessionStorage && !sessionStorage.getItem('IsThisFirstTime_Log_From_LiveServer')) {
+				console.log('Live reload enabled.');
+				sessionStorage.setItem('IsThisFirstTime_Log_From_LiveServer', true);
+			}
+		})();
+	}
+	else {
+		console.error('Upgrade your browser. This Browser is NOT supported WebSocket for Live-Reloading.');
+	}
+	// ]]>
+</script>
+</body>
+
+</html>
